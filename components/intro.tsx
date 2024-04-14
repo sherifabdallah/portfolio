8fa0d740cb26d715b542f6e840bb2b1a81f19f95
Sherif Abdullah <161659760+sherifabdallah@users.noreply.github.com>
2024-04-14 17:41:31 +0200

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -64,7 +64,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I'm Sherif.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span>{" "}
-        <br />I enjoy
+        <br />.I enjoy
         building <span className="italic">sites & apps</span>.<br /> My focus is{" "}
         <span className="underline">.NET Core & React</span>.
       </motion.h1>
