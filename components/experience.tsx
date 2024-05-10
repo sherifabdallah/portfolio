939b030620ffd9922dec18f957654e8d4967f496
Sherif Abdullah <161659760+sherifabdallah@users.noreply.github.com>
2024-05-10 03:06:15 +0300

    Update experience.tsx

--- components/experience.tsx
+++ components/experience.tsx
@@ -27,7 +27,7 @@ export default function Experience() {
                   theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
                 boxShadow: "none",
                 border: "1px solid rgba(0, 0, 0, 0.05)",
-                textAlign: "left",
+                textAlign: "right",
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
