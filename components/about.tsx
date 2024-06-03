82b84f1508f309f9a8e10601582b2d7a84255ac3
Sherif Abdullah <161659760+sherifabdallah@users.noreply.github.com>
2024-06-03 18:34:29 +0300

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -28,7 +28,7 @@ export default function About() {
         feeling of finally figuring out a solution to a problem. My core stack
         is{" "}
         <span className="font-medium">
-          React, Next.js, Node.js, and MongoDB
+          .NET Core and React
         </span>
         . I am always looking to
         learn new technologies. I am currently looking for a{" "}
